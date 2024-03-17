a55f26d2b0cfb6e3cbb780f1a109b3da758e2626
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-17 18:22:31 +0200

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -7,12 +7,6 @@ import {
   id = "shceduler"
 }
 
-data "archive_file" "zip_the_python_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/../../task-scheduler/"
-  output_path = "${path.module}/../../task-scheduler/scheduler.zip"
-}
-
 resource "aws_lambda_function" "terraform_lambda_func" {
 
   vpc_config {
@@ -26,10 +20,9 @@ resource "aws_lambda_function" "terraform_lambda_func" {
     }
   }
 
-  filename         = "${path.module}/../../task-scheduler/scheduler.zip"
   function_name    = "shceduler"
   role             = "arn:aws:iam::531190140983:role/service-role/testFc-role-l1r1aw1v"
   handler          = "index.lambda_handler"
   runtime          = "python3.8"
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+  source_path      = "${path.module}/../../task-scheduler/"
 }
