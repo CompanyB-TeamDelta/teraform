3b1ecdbdabb976a94c181a358344b8e21624585f
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-19 22:35:26 +0200

    Update main.tf

--- data-processor/main.tf
+++ data-processor/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 resource "aws_instance" "server" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
-  subnet_id = "vpc-0bba69cece42aa727"
-  key_name = "split-keys"
-  user_data = <<EOF
+  subnet_id     = "vpc-0bba69cece42aa727"
+  key_name      = "split-keys"
+  user_data     = <<EOF
 #!/bin/bash
 
 EOF
