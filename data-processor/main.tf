cd46c943bc88935ab7e7fa31251d39c70945065d
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-21 19:36:28 +0200

    Update main.tf

--- data-processor/main.tf
+++ data-processor/main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-backend "s3" {
-    bucket                  = "terraform-s3-state-hwnaukma2024"
-    key                     = "proj"
-    region                  = "us-east-1"
-  }
+terraform{
+  backend "s3" {
+      bucket                  = "terraform-s3-state-hwnaukma2024"
+      key                     = "proj"
+      region                  = "us-east-1"
+    }
+}
 
 resource "aws_instance" "server" {
 
