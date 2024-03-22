99b67f468ab69b5a63f0a697fa765f09b1e51ab8
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-22 20:42:01 +0200

    Update main.tf

--- data-processor/main.tf
+++ data-processor/main.tf
@@ -19,5 +19,13 @@ resource "aws_instance" "server" {
 #!/bin/bash
 scp -i key.pem data-processor.tar ec2-user@ec2-3-90-110-214.compute-1.amazonaws.com
 scp -i key.pem telegram-management.tar ec2-user@ec2-3-90-110-214.compute-1.amazonaws.com
+ssh -i key.pem ec2-user@ec2-54-204-100-254.compute-1.amazonaws.com
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker load < data-processor.tar
+sudo docker load < telegram-management.tar
+sudo docker run -d -p 8080:8080 --name data-processor
+sudo docker run -d -p 8088:8080 --name telegram-management
 EOF
 }
