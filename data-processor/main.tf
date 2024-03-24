7c9f01e82aa12958af5352a2270365249db0f361
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-24 11:34:10 +0200

    Update main.tf

--- data-processor/main.tf
+++ data-processor/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "CustomVPC" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "0.0.0.0/0"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
