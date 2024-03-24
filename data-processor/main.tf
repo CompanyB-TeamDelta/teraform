83228609336d071cab683e46c453d40941adb581
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-03-24 13:29:39 +0200

    Update main.tf

--- data-processor/main.tf
+++ data-processor/main.tf
@@ -78,6 +78,18 @@ ingress {
   }
 }
 
+resource "aws_route_table" "PublicRouteTable" {
+  vpc_id = aws_vpc.CustomVPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.GetIGW.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
 terraform{
   backend "s3" {
       bucket                  = "terraform-s3-state-hwnaukma2024"
