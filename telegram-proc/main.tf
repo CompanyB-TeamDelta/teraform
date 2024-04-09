7a53d3216627bf37882e67e3ab635e4e6ac6b7af
Vlad Zabrodskyi <74838322+Bac9nynkiH@users.noreply.github.com>
2024-04-09 18:37:01 +0300

    Update main.tf

--- telegram-proc/main.tf
+++ telegram-proc/main.tf
@@ -2,101 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_subnet" "PublicSubnet1" {
-  vpc_id     = aws_vpc.CustomVPC.id
-  cidr_block = "10.0.0.0/18"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "PublicSubnet1"
-    Type = "Public"
-  }
-}
-
-
-
-resource "aws_vpc" "CustomVPC" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "CustomVPC"
-           }
-}
-
-resource "aws_internet_gateway" "igw" { 
-vpc_id = aws_vpc.CustomVPC.id
-tags = { Name = "IGW" } 
-}
-
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "split"
-  description = "Allow http inbound traffic"
-  vpc_id      = aws_vpc.CustomVPC.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-ingress {
-    from_port   = 8088
-    to_port     = 8088
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "terraform-ec2-split-keys"
-  }
-}
-
-resource "aws_route_table" "PublicRouteTable" {
-  vpc_id = aws_vpc.CustomVPC.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  tags = {
-    Name = "PublicRouteTable"
-  }
-}
-
-resource "aws_route_table_association" "PublicSubnetRouteTableAssociation1" {
-  subnet_id      = aws_subnet.PublicSubnet1.id
-  route_table_id = aws_route_table.PublicRouteTable.id
-}
-
 terraform{
   backend "s3" {
       bucket                  = "terraform-s3-state-hwnaukma2024"
@@ -109,9 +14,9 @@ resource "aws_instance" "server" {
 
   ami           = "ami-0d7a109bf30624c99"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.PublicSubnet1.id
+  subnet_id     = "subnet-0c75f7a440c143dc5"
   key_name      = "split-keys"
-  security_groups = [aws_security_group.ec2_sg.id]
+  security_groups = ["sg-0c41e577818bc0a08"]
 
   tags = {
     Name = "telegram-proc"
